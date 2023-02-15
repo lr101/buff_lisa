b9605b6db284151fe8fc2869161bf9b55a1781b1
LR <Lukasr101@gmail.com>
2023-02-15 16:43:02 +0100

    small changes to fit ios

--- lib/1_BottomNavigationBar/navbar_ui.dart
+++ lib/1_BottomNavigationBar/navbar_ui.dart
@@ -26,35 +26,34 @@ class NavBarUI extends StatefulUI<BottomNavigationWidget, BottomNavigationWidget
             )
           ],
         ),
-        bottomNavigationBar: SizedBox(height: 58, child:BottomNavigationBar(
-          unselectedIconTheme: IconThemeData(color:  Provider.of<ThemeProvider>(context).getCustomTheme.c1,),
-          key: state.globalKey,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.group),
-              label: 'Groups',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_location_alt_outlined),
-              label: 'Camera',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.map_outlined),
-              label: 'Map',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.dynamic_feed),
-              label: 'Feed',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ),
-          ],
-          currentIndex: state.selectedIndex,
-          selectedItemColor: Provider.of<ThemeProvider>(context).getCustomTheme.c1,
-          onTap: state.onItemTapped,
-        )
+        bottomNavigationBar: BottomNavigationBar(
+                unselectedIconTheme: IconThemeData(color:  Provider.of<ThemeProvider>(context).getCustomTheme.c1,),
+                key: state.globalKey,
+                items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.group),
+                  label: 'Groups',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.add_location_alt_outlined),
+                  label: 'Camera',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.map_outlined),
+                  label: 'Map',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.dynamic_feed),
+                  label: 'Feed',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
+                ),
+              ],
+              currentIndex: state.selectedIndex,
+              selectedItemColor: Provider.of<ThemeProvider>(context).getCustomTheme.c1,
+              onTap: state.onItemTapped
       ),
     );
   }
