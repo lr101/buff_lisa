fb328910fc1bce0b9e7c514a49154166941f86ac
lukas <lukasr101@gmail.com>
2023-02-15 16:01:50 +0100

    delete ios folder

--- ios/Pods/Target Support Files/flutter_compass/flutter_compass-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_compass : NSObject
-@end
-@implementation PodsDummy_flutter_compass
-@end
--- ios/Pods/Target Support Files/google_mobile_ads/google_mobile_ads-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_google_mobile_ads : NSObject
-@end
-@implementation PodsDummy_google_mobile_ads
-@end
