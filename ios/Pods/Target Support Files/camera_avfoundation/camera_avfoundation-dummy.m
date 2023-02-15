89584891ec8cee05fb52f0ddd1edb19f231010ea
Lukas Reim <LR@0yma8-17.ten.whka.de>
2023-02-15 15:48:59 +0100

    delete ios librarys

--- ios/Pods/Target Support Files/camera_avfoundation/camera_avfoundation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_camera_avfoundation : NSObject
-@end
-@implementation PodsDummy_camera_avfoundation
-@end
--- ios/Pods/Target Support Files/images_picker/images_picker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_images_picker : NSObject
-@end
-@implementation PodsDummy_images_picker
-@end
