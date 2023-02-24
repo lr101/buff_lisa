e06e2cccad1d5c5e8a30302f5657d96ddd53a2fd
LR <Lukasr101@gmail.com>
2023-02-24 14:03:45 +0100

    - make transparent and change text color to dark and light mode

--- ios/Runner/ListTileNativeAdFactory.swift
+++ ios/Runner/ListTileNativeAdFactory.swift
@@ -18,7 +18,26 @@ class ListTileNativeAdFactory : FLTNativeAdFactory {
         nativeAdView.iconView!.isHidden = nativeAd.icon == nil
 
         nativeAdView.callToActionView?.isUserInteractionEnabled = false
+        
+        (nativeAdView.storeView as? UILabel)?.text = nativeAd.store
+        nativeAdView.storeView?.isHidden = nativeAd.store == nil
 
+        (nativeAdView.priceView as? UILabel)?.text = nativeAd.price
+        nativeAdView.priceView?.isHidden = nativeAd.price == nil
+
+        (nativeAdView.advertiserView as? UILabel)?.text = nativeAd.advertiser
+        nativeAdView.advertiserView?.isHidden = nativeAd.advertiser == nil
+
+        
+        let brightness = customOptions?["brightness"]
+        let color = brightness as! Bool ? UIColor(red: 1, green: 1, blue: 1, alpha: 1): UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        (nativeAdView.headlineView as! UILabel).textColor = color
+        (nativeAdView.bodyView as! UILabel).textColor = color
+        (nativeAdView.storeView as? UILabel)?.textColor = color
+        (nativeAdView.priceView as? UILabel)?.textColor = color
+        (nativeAdView.advertiserView as? UILabel)?.textColor = color
+        
+        
         nativeAdView.nativeAd = nativeAd
 
         return nativeAdView
